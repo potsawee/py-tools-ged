fae85898421998825349b7b0fedf95a8943e8949
Potsawee Manakul <m.potsawee@gmail.com>
2018-12-17 17:51:54 +0000

    add formatcovert

---
+++ gedformatconvert.py
@@ -0,0 +1,36 @@
+import sys
+
+def one_word_to_one_sentence(input, output):
+    with open(input, 'r') as f1:
+        with open(output, 'w') as f2:
+            newline = []
+            for line in f1:
+                if line == '\n':
+                    if len(newline) > 0:
+                        f2.write(' '.join(newline) + '\n')
+                    newline = []
+                else:
+                    newline.append(line.strip('\n'))
+    print('convert one_word_to_one_sentence done!')
+
+
+
+def one_sentence_to_one_word(input, output):
+    pass
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python3 gedformatconvert.py [1/2] input output")
+        return
+
+    convert_type = sys.argv[1]
+    input = sys.argv[2]
+    output = sys.argv[3]
+
+    if convert_type == '1':
+        one_word_to_one_sentence(input, output)
+    elif convert_type == '2':
+        one_sentence_to_one_word(input, output)
+
+if __name__ == '__main__':
+    main()

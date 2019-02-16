d56635c1453ccd089fae8dc47e75bf499d305e78
Potsawee Manakul <m.potsawee@gmail.com>
2019-02-16 21:39:10 +0000

    add gec to ged input

--- gedformatconvert.py
+++ gedformatconvert.py
@@ -10,13 +10,35 @@ def one_word_to_one_sentence(input, output):
                         f2.write(' '.join(newline) + '\n')
                     newline = []
                 else:
-                    newline.append(line.strip('\n'))
+                    word = line.strip().split()[0]
+                    # ----------- Filter ----------- #
+                    # for swb work - 8 Feb 2019
+                    # disf = line.strip().split()[1]
+                    # if disf != 'O':
+                    #     continue
+                    # ------------------------------ #
+                    newline.append(word)
     print('convert one_word_to_one_sentence done!')
 
 
 
 def one_sentence_to_one_word(input, output):
-    pass
+    with open(input, 'r') as f1:
+        with open(output, 'w') as f2:
+            newline = []
+            for line in f1:
+                # if line == '\n':
+                #     if len(newline) > 0:
+                #         f2.write(' '.join(newline) + '\n')
+                #     newline = []
+                # else:
+                #     word = line.strip().split()[0]
+                #     newline.append(word)
+                words = line.split()
+                for word in words:
+                    f2.write(word + '\n')
+                f2.write('\n')
+    print('convert one_sentence_to_one_word done!')
 
 def main():
     if len(sys.argv) != 4:
